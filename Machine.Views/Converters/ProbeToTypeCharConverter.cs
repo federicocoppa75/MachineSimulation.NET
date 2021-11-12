fd89b262dbebb1691812d6bcb4319e86f2c7ae1e
Federico Coppa <federicocoppa75@gmail.com>
2021-11-12 17:19:21 +0100

    dev probe type in list view

---
+++ Machine.Views/Converters/ProbeToTypeCharConverter.cs
@@ -0,0 +1,60 @@
+﻿using Machine.ViewModels.Interfaces.Probing;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Machine.Views.Converters
+{
+    [ContentProperty("Values")]
+    class ProbeToTypeCharConverter : IValueConverter
+    {
+        public List<ProbeToTypeCharConverterItem> Values { get; set; } = new List<ProbeToTypeCharConverterItem>();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            object result = null;
+
+            if (value is IProbe v)
+            {
+                var interfaces = v.GetType().GetInterfaces();
+
+
+                foreach (var item in interfaces)
+                {
+                    var end = false;
+
+                    foreach (var option in Values)
+                    {
+                        if(string.Compare(item.Name, option.When) == 0)
+                        {
+                            result = option.Then;
+                            end = true;
+                            break;
+                        }
+                    }
+
+                    if (end) break;
+                }
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [ContentProperty("Then")]
+    class ProbeToTypeCharConverterItem
+    {
+        public string When { get; set; }
+        public object Then { get; set; }
+    }
+}
